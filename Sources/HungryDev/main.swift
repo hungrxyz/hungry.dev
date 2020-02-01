333f59a252ca833613e9ae77e568a4c7d7cc393b
marko <marko@hungry.dev>
2020-02-01 14:55:47 +0100

    fixes date formatter locale to en_US

--- Sources/HungryDev/main.swift
+++ Sources/HungryDev/main.swift
@@ -9,7 +9,17 @@ import Foundation
 import Publish
 import SplashPublishPlugin
 
+public extension Plugin {
+    /// Fixing date formatter locale to `en_US` since all dates for posts are in `yyyy-MM-dd HH:mm`.
+    static func usLocaleDateFormatter() -> Self {
+        Plugin(name: "US Locale Date Formatter") { context in
+            context.dateFormatter.locale = Locale(identifier: "en_US")
+        }
+    }
+}
+
 try HungryDev().publish(using: [
+    .installPlugin(.usLocaleDateFormatter()),
     .installPlugin(.splash(withClassPrefix: "")),
     .addMarkdownFiles(),
     .sortItems(by: \.date, order: .descending),
