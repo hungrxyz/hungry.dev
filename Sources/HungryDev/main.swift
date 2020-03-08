fd2ab92fac29a0112b403a0418d69f2490163a38
marko <marko@hungry.dev>
2020-03-09 00:11:25 +0100

    use pygments directly

--- Sources/HungryDev/main.swift
+++ Sources/HungryDev/main.swift
@@ -7,7 +7,48 @@
 
 import Foundation
 import Publish
-import PygmentsPublishPlugin
+import Ink
+import ShellOut
+
+public extension Plugin {
+    static func pygmentize() -> Self {
+        Plugin(name: "Pygments") { context in
+            context.markdownParser.addModifier(
+                .pygmentizeCodeBlocks()
+            )
+        }
+    }
+}
+
+public extension Modifier {
+    static func pygmentizeCodeBlocks() -> Self {
+        return Modifier(target: .codeBlocks) { html, markdown in
+
+            let begin = markdown.components(separatedBy: .newlines).first ?? "```"
+            let language = begin.dropFirst("```".count)
+
+            var markdown = markdown.dropFirst(begin.count)
+
+            guard language != "no-highlight" else {
+                return html
+            }
+
+            markdown = markdown
+                .drop(while: { !$0.isNewline })
+                .dropFirst()
+                .dropLast("\n```".count)
+
+            let markdownString = String(markdown).replacingOccurrences(of: "\"", with: "\\\"")
+            let cmd = #"echo "\#(markdownString)" | pygmentize -s -l \#(String(language)) -f html -O nowrap"#
+
+            if let highlighted = try? shellOut(to: cmd) {
+                return "<pre><code>" + highlighted + "\n</code></pre>"
+            }
+            return "<pre><code>" + String(markdown) + "\n</code></pre>"
+        }
+    }
+}
+
 
 public extension Plugin {
     /// Fixing date formatter locale to `en_US` since all dates for posts are in `yyyy-MM-dd HH:mm`.
